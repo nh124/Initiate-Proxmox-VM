87e585884709260b703a64ddb18cf40b2d0841e5
Nur Haque <nur.haque99@gmail.com>
2026-09-25 17:45:37 -0400

    Updated storage pool back to local

--- variables.tf
+++ variables.tf
@@ -55,7 +55,7 @@ variable "datastore_id" {
 variable "image_datastore_id" {
   description = "Proxmox datastore used to store the downloaded image"
   type        = string
-  default     = "big-4tb"
+  default     = "local"
 }
 
 variable "vm_username" {
