268892eb2fc9a560599d11ee4d4df93b361825bf
Nur Haque <nur.haque99@gmail.com>
2026-09-26 15:06:43 -0400

    Apply talos configuration

--- talos.tf
+++ talos.tf
@@ -25,3 +25,16 @@ data "talos_machine_configuration" "controlplane" {
     })
   ]
 }
+
+resource "talos_machine_configuration_apply" "controlplane" {
+  client_configuration = talos_machine_secrets.this.client_configuration
+
+  machine_configuration_input =
+    data.talos_machine_configuration.controlplane.machine_configuration
+
+  node = var.control_plane_ip
+
+  depends_on = [
+    module.proxmox_vm
+  ]
+}
