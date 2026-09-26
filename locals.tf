7677e02b4e83d4e1b6e62b71d1bb271cea8200aa
Nur Haque <nur.haque99@gmail.com>
2026-09-25 22:15:56 -0400

    Test coomplete

--- locals.tf
+++ locals.tf
@@ -6,41 +6,41 @@ locals {
       ram    = 4096
       disk   = 2000
       bridge = "vmbr0"
+    },
+    {
+      name   = "vaultwarden"
+      cpu    = 4
+      ram    = 6144
+      disk   = 100
+      bridge = "vmbr0"
+    },
+    {
+      name   = "immich"
+      cpu    = 4
+      ram    = 6144
+      disk   = 2020
+      bridge = "vmbr0"
+    },
+    {
+      name   = "n8n"
+      cpu    = 2
+      ram    = 2048
+      disk   = 10
+      bridge = "vmbr0"
+    },
+    {
+      name   = "jellyfin"
+      cpu    = 2
+      ram    = 2048
+      disk   = 2016
+      bridge = "vmbr0"
+    },
+    {
+      name   = "mattermost"
+      cpu    = 2
+      ram    = 4048
+      disk   = 8
+      bridge = "vmbr0"
     }
-    # {
-    #   name   = "vaultwarden"
-    #   cpu    = 4
-    #   ram    = 6144
-    #   disk   = 100
-    #   bridge = "vmbr0"
-    # },
-    # {
-    #   name   = "immich"
-    #   cpu    = 4
-    #   ram    = 6144
-    #   disk   = 2020
-    #   bridge = "vmbr0"
-    # },
-    # {
-    #   name   = "n8n"
-    #   cpu    = 2
-    #   ram    = 2048
-    #   disk   = 10
-    #   bridge = "vmbr0"
-    # },
-    # {
-    #   name   = "jellyfin"
-    #   cpu    = 2
-    #   ram    = 2048
-    #   disk   = 2016
-    #   bridge = "vmbr0"
-    # },
-    # {
-    #   name   = "mattermost"
-    #   cpu    = 2
-    #   ram    = 4048
-    #   disk   = 8
-    #   bridge = "vmbr0"
-    # }
   ]
 }
