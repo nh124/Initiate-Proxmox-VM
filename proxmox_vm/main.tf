359c5677e23dc7ae4550f7e075e2b42bf49a6e40
Nur Haque <nur.haque99@gmail.com>
2026-09-26 13:43:13 -0400

    Fixed plan error

--- proxmox_vm/main.tf
+++ proxmox_vm/main.tf
@@ -1,11 +1,11 @@
 resource "proxmox_virtual_environment_vm" "talos" {
-  vm_id     = var.vm_id
-  name      = var.name
-  node_name = var.node_name
-  started        = true
+  vm_id           = var.vm_id
+  name            = var.name
+  node_name       = var.node_name
+  started         = true
   stop_on_destroy = true
-  machine = "q35"
-  bios    = "ovmf"
+  machine         = "q35"
+  bios            = "ovmf"
   cpu {
     type    = "host"
     cores   = var.cores
@@ -22,7 +22,7 @@ resource "proxmox_virtual_environment_vm" "talos" {
     discard      = "on"
   }
   cdrom {
-    file_id = proxmox_virtual_environment_download_file.talos_iso
+    file_id = proxmox_virtual_environment_download_file.talos_iso.id
   }
   network_device {
     bridge      = var.bridge
--- proxmox_vm/proxmox_iso_downloader.tf
+++ proxmox_vm/proxmox_iso_downloader.tf
@@ -1,7 +1,7 @@
 resource "proxmox_virtual_environment_download_file" "talos_iso" {
   content_type = "iso"
   datastore_id = "local"
-  node_name = var.node_name
-  url = "https://github.com/siderolabs/talos/releases/download/v1.14.0/metal-amd64.iso"
-  file_name = "talos-v1.14.0-metal-amd64.iso"
+  node_name    = var.node_name
+  url          = "https://github.com/siderolabs/talos/releases/download/v1.14.0/metal-amd64.iso"
+  file_name    = "talos-v1.14.0-metal-amd64.iso"
 }
