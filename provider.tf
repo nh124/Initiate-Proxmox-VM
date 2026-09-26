a8d5f1f757eff33d85e4ea334bcc07202a872319
Nur Haque <nur.haque99@gmail.com>
2026-09-25 20:16:39 -0400

    Fixed build error

--- provider.tf
+++ provider.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    proxmox = {
-      source  = "bpg/proxmox"
-      version = "~> 0.83"
-    }
-  }
-}
-
 provider "proxmox" {
   endpoint  = var.proxmox_endpoint
   insecure  = true
