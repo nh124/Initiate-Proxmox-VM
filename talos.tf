a0ec2d6c58afbf2958a12afb714bd2635cd99744
Nur Haque <nur.haque99@gmail.com>
2026-09-26 15:03:01 -0400

    Generate the Talos machine configuration

--- talos.tf
+++ talos.tf
@@ -1,1 +1,27 @@
 resource "talos_machine_secrets" "this" {}
+
+data "talos_machine_configuration" "controlplane" {
+  cluster_name     = var.cluster_name
+  machine_type     = "controlplane"
+  cluster_endpoint = var.cluster_endpoint
+  machine_secrets  = talos_machine_secrets.this.machine_secrets
+  talos_version    = var.talos_version
+
+  config_patches = [
+    yamlencode({
+      machine = {
+        install = {
+          disk = "/dev/sda"
+        }
+
+        network = {
+          hostname = "talos-cp-01"
+        }
+      }
+
+      cluster = {
+        allowSchedulingOnControlPlanes = true
+      }
+    })
+  ]
+}
